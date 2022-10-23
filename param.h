4792e9c05f89fb0d2a1bc44255de15a4020aad75
Abhilash <jindal.abhilash@gmail.com>
2022-10-24 00:48:18 +0530

    OS that boots and exits gracefully!

---
+++ param.h
@@ -0,0 +1,5 @@
+#define KSTACKSIZE 4096  // size of per-process kernel stack
+#define NCPU          8  // maximum number of CPUs
+#define MAXOPBLOCKS  10  // max # of blocks any FS op writes
+#define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
+#define FSSIZE       1000  // size of file system in blocks

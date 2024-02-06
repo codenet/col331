42f9cff31d5cd830f681852f8479cfabbe1548fc
Anish Banerjee <97676070+AB271202@users.noreply.github.com>
2024-02-06 20:46:39 +0530

    Removed dead code in lapic.c and defs.h (#14)
    * Removed dead code from lapic.c
    
    The following two functions were not being used in this branch:
    // Acknowledge interrupt.
    void
    lapiceoi(void)
    {
      if(lapic)
        lapicw(EOI, 0);
    }
    
    // Spin for a given number of microseconds.
    // On real hardware would want to tune this dynamically.
    void
    microdelay(int us)
    {
    }
    
    * Removed dead definitions in defs.h
    
    These functions are not used
    void            lapiceoi(void);
    void            microdelay(int);

--- defs.h
+++ defs.h
@@ -13,10 +13,8 @@ void            ioapicinit(void);
 void            cmostime(struct rtcdate *r);
 int             lapicid(void);
 extern volatile uint*    lapic;
-void            lapiceoi(void);
 void            lapicinit(void);
 void            lapicstartap(uchar, uint);
-void            microdelay(int);
 
 // mp.c
 extern int      ismp;

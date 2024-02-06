dafbea3227b229cdead50d997b3718aefc8099b6
KushagraGupta02 <109025706+KushagraGupta02@users.noreply.github.com>
2024-02-06 20:53:12 +0530

    Update stat.h [Removed Dead Code] (#20)

--- stat.h
+++ stat.h
@@ -1,7 +1,3 @@
-#define T_DIR  1   // Directory
-#define T_FILE 2   // File
-#define T_DEV  3   // Device
-
 struct stat {
   short type;  // Type of file
   int dev;     // File system's disk device

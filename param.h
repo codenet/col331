42ff1fd1659c7294759e8d3742aef9af50a0a3eb
Anoop Singh <cs1230459@iitd.ac.in>
2026-02-14 14:14:47 +0530

    Remove duplicate code

--- param.h
+++ param.h
@@ -3,8 +3,6 @@
 #define NINODE       50  // maximum number of active i-nodes
 #define ROOTDEV       1  // device number of file system root disk
 #define MAXOPBLOCKS  10  // max # of blocks any FS op writes
-#define NINODE       50  // maximum number of active i-nodes
-#define ROOTDEV       1  // device number of file system root disk
 #define LOGSIZE      0  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks

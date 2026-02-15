70f828fb442acb425fb30ec681b8076f40d20fb5
Akhilesh Agrawal <146083440+akhil155107@users.noreply.github.com>
2026-02-15 19:42:38 +0530

    Remove lapicstartap dead declaration
    This function is declared but never defined in the codebase.

--- defs.h
+++ defs.h
@@ -63,7 +63,6 @@ int             lapicid(void);
 extern volatile uint*    lapic;
 void            lapiceoi(void);
 void            lapicinit(void);
-void            lapicstartap(uchar, uint);
 void            microdelay(int);
 
 // log.c

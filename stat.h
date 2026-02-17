bfb4d31caa306f95ffdd80dd7da12143824c987f
KATARU SIRI DHARAN <cs1230771@iitd.ac.in>
2026-02-17 12:31:37 +0530

    Delete stat.h- file not used in p6-ide.(unnecessary code)

--- stat.h
+++
@@ -1,7 +0,0 @@
-struct stat {
-  short type;  // Type of file
-  int dev;     // File system's disk device
-  uint ino;    // Inode number
-  short nlink; // Number of links to file
-  uint size;   // Size of file in bytes
-};

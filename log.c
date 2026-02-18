e5dbb9248c3ec47ef75168829fd55d88672f35ac
Rachit-Bhalani <rachitbhalani@gmail.com>
2026-02-18 11:06:21 +0530

    Refactor log_write for improved transaction handling
    Refactor log_write function to check for log absorption before panicking on transaction size.

--- log.c
+++ log.c
@@ -174,14 +174,13 @@ void
 log_write(struct buf *b)
 {
   int i;
-
-  if (log.lh.n >= LOGSIZE || log.lh.n >= log.size - 1)
-    panic("too big a transaction");
-
+  // check for log absorption before panicking
   for (i = 0; i < log.lh.n; i++) {
     if (log.lh.block[i] == b->blockno)   // log absorbtion
       break;
   }
+  if (i==log.lh.n && (log.lh.n >= LOGSIZE || log.lh.n >= log.size - 1))
+    panic("too big a transaction");
   log.lh.block[i] = b->blockno;
   if (i == log.lh.n)
     log.lh.n++;

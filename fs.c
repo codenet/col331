d39bda6b25e7dfa991a71ef95c5f334fc1bf65dd
root <root@LAPTOP-VK1T6K3D.localdomain>
2026-03-19 10:15:30 +0000

    spinlocks replaced pushcli/popcli

--- fs.c
+++ fs.c
@@ -18,7 +18,7 @@
 #include "fs.h"
 #include "buf.h"
 #include "file.h"
-#include "spinlock.h"
+// #include "spinlock.h"
 
 #define min(a, b) ((a) < (b) ? (a) : (b))
 static void itrunc(struct inode*);
@@ -111,14 +111,14 @@ bfree(int dev, uint b)
 //
 
 struct {
-  struct spinlock lock; 
+  // struct spinlock lock; 
   struct inode inode[NINODE];
 } icache;
 
 void
 iinit(int dev)
 {
-  initlock(&icache.lock, "icache");
+  // initlock(&icache.lock, "icache");
   readsb(dev, &sb);
   cprintf("sb: size %d nblocks %d ninodes %d nlog %d logstart %d\
  inodestart %d bmap start %d\n", sb.size, sb.nblocks,
@@ -159,25 +159,29 @@ ialloc(uint dev, short type)
 void
 iput(struct inode *ip)
 {
-  acquire(&icache.lock);
-  
+  // acquire(&icache.lock);
+  pushcli();
+
   if(ip->valid && ip->nlink == 0){
     int r = ip->ref;
     if(r == 1){
       // inode has no links and no other references: truncate and free.
-      release(&icache.lock);
+      // release(&icache.lock);
+      popcli();
       
       itrunc(ip);
       ip->type = 0;
       iupdate(ip);
       ip->valid = 0;
       
-      acquire(&icache.lock);
+      // acquire(&icache.lock);
+      pushcli();
     }
   }
   
   ip->ref--;
-  release(&icache.lock); // (Final release)
+  // release(&icache.lock); 
+  popcli();
 }
 
 // Copy a modified in-memory inode to disk.
@@ -209,14 +213,16 @@ iget(uint dev, uint inum)
 {
   struct inode *ip, *empty;
 
-  acquire(&icache.lock);
+  // acquire(&icache.lock);
+  pushcli();
 
   // Is the inode already cached?
   empty = 0;
   for(ip = &icache.inode[0]; ip < &icache.inode[NINODE]; ip++){
     if(ip->ref > 0 && ip->dev == dev && ip->inum == inum){
       ip->ref++;
-      release(&icache.lock);
+      // release(&icache.lock);
+      popcli();
       return ip;
     }
     if(empty == 0 && ip->ref == 0)    // Remember empty slot.
@@ -233,7 +239,8 @@ iget(uint dev, uint inum)
   ip->ref = 1;
   ip->valid = 0;
 
-  release(&icache.lock);
+  // release(&icache.lock);
+  popcli();
 
   return ip;
 }
--- proc.c
+++ proc.c
@@ -5,10 +5,10 @@
 #include "mmu.h"
 #include "x86.h"
 #include "proc.h"
-#include "spinlock.h"
+// #include "spinlock.h"
 
 struct {
-  struct spinlock lock;
+  // struct spinlock lock;
   struct proc proc[NPROC];
 } ptable;
 
@@ -47,17 +47,20 @@ allocproc(void)
   struct proc *p;
   char *sp;
 
-  acquire(&ptable.lock);
+  // acquire(&ptable.lock);
+  pushcli();
   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
     if(p->state == UNUSED)
       goto found;
-  release(&ptable.lock);
+  // release(&ptable.lock);
+  popcli();
   return 0;
 
 found:
   p->state = EMBRYO;
   p->pid = nextpid++;
-  release(&ptable.lock);
+  // release(&ptable.lock);
+  popcli();
 
   if((p->offset = kalloc()) == 0){
     p->state = UNUSED;
@@ -99,7 +102,7 @@ found:
 void
 pinit(void)
 {
-  initlock(&ptable.lock, "ptable");
+  // initlock(&ptable.lock, "ptable");
   
   struct proc *p;
   extern char _binary_initcode_start[], _binary_initcode_size[];
@@ -143,7 +146,8 @@ scheduler(void)
     // Enable interrupts on this processor.
     sti();
 
-    acquire(&ptable.lock);
+    // acquire(&ptable.lock);
+    pushcli();
     // Loop over process table looking for process to run.
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
@@ -156,7 +160,8 @@ scheduler(void)
       switchuvm(p);
       swtch(&(c->scheduler), p->context);
     }
-    release(&ptable.lock);
+    // release(&ptable.lock);
+    popcli();
   }
 }
 
@@ -187,10 +192,12 @@ sched(void)
 void
 yield(void)
 {
-  acquire(&ptable.lock);
+  // acquire(&ptable.lock);
+  pushcli();
   myproc()->state = RUNNABLE;
   sched();
-  release(&ptable.lock);
+  // release(&ptable.lock);
+  popcli();
 }
 
 // A fork child's very first scheduling by scheduler()
@@ -199,7 +206,8 @@ void
 forkret(void)
 {
   // Release the lock held by the scheduler
-  release(&ptable.lock);
+  // release(&ptable.lock);
+  popcli();
   // Return to "caller", actually trapret (see allocproc).
 }
 
@@ -216,7 +224,8 @@ sleep(void *chan)
 
   // Must acquire ptable.lock in order to
   // change p->state and then call sched.
-  acquire(&ptable.lock);
+  // acquire(&ptable.lock);
+  pushcli();
   // Go to sleep.
   p->chan = chan;
   p->state = SLEEPING;
@@ -226,7 +235,8 @@ sleep(void *chan)
   // Tidy up.
   p->chan = 0;
 
-  release(&ptable.lock);
+  // release(&ptable.lock);
+  popcli();
 }
 
 // Wake up all processes sleeping on chan.
@@ -245,9 +255,11 @@ wakeup1(void *chan)
 void
 wakeup(void *chan)
 {
-  acquire(&ptable.lock);
+  // acquire(&ptable.lock);
+  pushcli();
   wakeup1(chan);
-  release(&ptable.lock);
+  // release(&ptable.lock);
+  popcli();
 }
 
 void
@@ -262,7 +274,8 @@ procdump(void)
   };
   struct proc *p;
   char *state;
-  acquire(&ptable.lock);
+  // acquire(&ptable.lock);
+  pushcli();
   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
     if(p->state == UNUSED)
       continue;
@@ -273,5 +286,6 @@ procdump(void)
     cprintf("%d %s %s", p->pid, state, p->name);
     cprintf("\n");
   }
-  release(&ptable.lock);
+  // release(&ptable.lock);
+  popcli();
 }

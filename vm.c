9ae20c18d7ea227363c760eae2810c0363e6bae4
sudoheckbeluga <157679049+sudoheckbeluga@users.noreply.github.com>
2026-03-20 19:06:38 +0530

    Update segment sizes for user code and data

--- vm.c
+++ vm.c
@@ -17,7 +17,7 @@ seginit(void)
   c = &cpus[cpuid()];
   c->gdt[SEG_KCODE] = SEG(STA_X|STA_R, 0, 0xffffffff, 0);
   c->gdt[SEG_KDATA] = SEG(STA_W, 0, 0xffffffff, 0);
-  c->gdt[SEG_UCODE] = SEG(STA_X|STA_R, STARTPROC, PROCSIZE, 0);
-  c->gdt[SEG_UDATA] = SEG(STA_W, STARTPROC, PROCSIZE, 0);
+  c->gdt[SEG_UCODE] = SEG(STA_X|STA_R, STARTPROC, (PROCSIZE << 12), 0);
+  c->gdt[SEG_UDATA] = SEG(STA_W, STARTPROC, (PROCSIZE << 12), 0);
   lgdt(c->gdt, sizeof(c->gdt));
 }

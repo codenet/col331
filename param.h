59d296b446245b0023f39568a460671a10becefa
Aayush <aayushprasad@Aayushs-MacBook-Air.local>
2026-04-13 18:19:23 +0530

    Remove duplicate NINODE and ROOTDEV defines in param.h

--- param.h
+++ param.h
@@ -6,9 +6,7 @@
 #define MAXOPBLOCKS  10  // max # of blocks any FS op writes
 #define NOFILE       16  // open files per process
 #define NFILE       100  // open files per system
-#define NINODE       50  // maximum number of active i-nodes
 #define NDEV         10  // maximum major device number
-#define ROOTDEV       1  // device number of file system root disk
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks

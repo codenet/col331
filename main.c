8b594a66bf104737194abb2829e05f77103c9c01
Amber-Agarwal <154735820+Amber-Agarwal@users.noreply.github.com>
2026-04-29 05:59:35 +0530

    Fix: Memory leak because buffer was not released (#83)
    The buffer was not released after being used in welcome function which is a leak. Fixed by adding brelse for both b0 and b1

--- main.c
+++ main.c
@@ -11,11 +11,12 @@ welcome(void) {
   struct buf *b0 = bread(1, 0);
   for(int i=0; i < BSIZE; i++)
     consputc(b0->data[i]);
-  
+  brelse(b0);
   struct buf *b1 = bread(1, 1);
   cprintf("After preparing fs.img, we have rebooted %d times\n", b1->data[0]);
   b1->data[0] = b1->data[0] + 1;
   bwrite(b1);
+  brelse(b1);
 }
 
 // Bootstrap processor starts running C code here.

f3f4f69af9c698a7338747ece359ad27f9e557d6
MANVENDRA RAJPUROHIT <145118895+manvendrarajpurohit@users.noreply.github.com>
2026-05-09 20:02:53 +0530

    bug p28-kill:set killed=1 on unexpected user-space traps (#355)

--- trap.c
+++ trap.c
@@ -66,6 +66,12 @@ trap(struct trapframe *tf)
               tf->trapno, cpuid(), tf->eip, rcr2());
       panic("trap");
     }
+    // In user space, assume process misbehaved.
+    cprintf("pid %d %s: trap %d err %d on cpu %d "
+            "eip 0x%x addr 0x%x--kill proc\n",
+            myproc()->pid, myproc()->name, tf->trapno,
+            tf->err, cpuid(), tf->eip, rcr2());
+    myproc()->killed = 1;
   }
 
   // Force process to exit if it has been killed and is in user space.

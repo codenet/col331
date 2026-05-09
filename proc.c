dd25559f237d106acce702185be86eb55d8cfb57
Sai Kaushik S <saikaushik609@gmail.com>
2026-05-09 21:37:54 +0530

    Merging fix from #293

--- proc.c
+++ proc.c
@@ -122,17 +122,20 @@ scheduler(void)
   c->proc = 0;
   
   for(;;){
+    sti();
     // Loop over process table looking for process to run.
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
         continue;
 
-      // Switch to chosen process. 
+      // Switch to chosen process.
+      cli(); 
       c->proc = p;
       p->state = RUNNING;
 
       switchuvm(p);
       swtch(&(c->scheduler), p->context);
+      sti();
     }
   }
 }
